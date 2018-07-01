f78fecae92c4d33a96b6d668e64263f2e5f89150
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-01 19:51:16 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "setimmediate";
+import buffer from "buffer";
+window.Buffer = buffer.Buffer;
+
+import sass from "sass";
+
+class Darter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      scss: ".foo { .baz { color: red} }",
+      css: ""
+    };
+  }
+  componentDidMount() {
+    this.sync();
+  }
+  handleChange = e => {
+    console.log(e.target.value);
+    this.setState(
+      {
+        scss: e.target.value
+      },
+      () => {
+        this.sync();
+      }
+    );
+  };
+  sync() {
+    try {
+      const css = sass
+        .renderSync({
+          data: this.state.scss
+        })
+        .css.toString();
+      this.setState({
+        css
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <textarea onChange={this.handleChange} value={this.state.scss} />
+        <pre>
+          <code>{this.state.css}</code>
+        </pre>
+      </div>
+    );
+  }
+}
+function App() {
+  return (
+    <div className="App">
+      <Darter />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
